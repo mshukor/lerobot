a216a864fabf37e84168a299b65196ae7ee22dd9
mshukor <mustafa.shukor97@gmail.com>
2025-05-30 10:34:18 +0200

    vla stuff for control lerobot

--- lerobot/common/utils/control_utils.py
+++ lerobot/common/utils/control_utils.py
@@ -260,7 +260,8 @@ def control_loop(
         else:
             observation = robot.capture_observation()
             action = None
-
+            observation['task'] = [single_task]
+            observation["robot_type"] = ["so101"] # FIXME(mshukor): hard coded
             if policy is not None:
                 pred_action = predict_action(
                     observation, policy, get_safe_torch_device(policy.config.device), policy.config.use_amp
